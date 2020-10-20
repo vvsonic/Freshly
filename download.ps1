0f436133cadeee0edae1d3f8837939c86bee987c
vvsonic <73136664+vvsonic@users.noreply.github.com>
2020-10-19 19:43:03 -0700

    Add files via upload

--- download.ps1
+++ download.ps1
@@ -1,7 +1,7 @@
 Start-Process Powershell -Verb runAs
 Set-ExecutionPolicy remotesigned
 function downloadbuilder {
-Invoke-WebRequest -Uri "http://sc1.soniccloud.org/portal/s/0148648199101696450934.zip" -OutFile "C:\Freshly\PCdeploy.zip"
+Invoke-WebRequest -Uri "https://github.com/vvsonic/Freshly/archive/main.zip" -OutFile "C:\Freshly\PCdeploy.zip"
 Expand-Archive C:\Freshly\PCdeploy.zip -DestinationPath C:\Freshly\
 }
 downloadbuilder
