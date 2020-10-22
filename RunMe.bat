e6fdf86839217ed402d7880507f3ad2061ab20e8
vvsonic <73136664+vvsonic@users.noreply.github.com>
2020-10-21 23:18:14 -0700

    Add files via upload

--- RunMe.bat
+++ RunMe.bat
@@ -1,8 +1,10 @@
+REM Create a directory on the C drive to store the scripts from the repository
+mkdir c:\Freshly
 REM Enable execution policy to allow running of scripts
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList 'set-executionpolicy remotesigned' -Verb RunAs}"
 REM Download the most up to date version of the script from the repository. download.ps1 must be in the same directory as RunMe.bat
-powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
+Powershell.exe -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
 REM The download.ps1 auto extracts the contents to c:\Freshly. Wait 10 seconds for this to complete before executing the PCSetup.ps1 script
 timeout 10
 REM Execute the final build script
-powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\Freshly\PCdeploy\PCSetup.ps1\"' -verb RunAs}"
+powershell.exe -command "&{start-process powershell -ArgumentList '-noprofile -file \"C:\Freshly\Freshly-main\PCSetup.ps1\"' -verb RunAs}"
