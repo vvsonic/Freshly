9099948232149472c263569b64191ff1b9ab207b
vvsonic <73136664+vvsonic@users.noreply.github.com>
2020-10-27 11:22:02 -0700

    Add files via upload

--- download.ps1
+++ download.ps1
@@ -1,6 +1,15 @@
 Set-ExecutionPolicy Bypass
+$FileName1 = "C:\Freshly\Freshly.Zip"
+$FileName2 = "C:\Freshly\Freshly-main"
+    If ((test-path $FileName1) -and (test-path $FileName2))
+    {
+      Remove-Item $FileName1 -Force
+      Remove-Item $FileName2 -Force    
+    }
+
 function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/vvsonic/Freshly/archive/main.zip" -OutFile "C:\Freshly\Freshly.zip"
-Expand-Archive C:\Freshly\Freshly.zip -DestinationPath C:\Freshly\
+
+     Invoke-WebRequest -Uri "https://github.com/vvsonic/Freshly/archive/main.zip" -OutFile "C:\Freshly\Freshly.zip"
+     Expand-Archive C:\Freshly\Freshly.zip -DestinationPath C:\Freshly\
 }
 downloadbuilder
