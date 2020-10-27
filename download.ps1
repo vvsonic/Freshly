ecc34994e0fd88d897be18a32f98a933e8e71bbd
vvsonic <73136664+vvsonic@users.noreply.github.com>
2020-10-27 09:49:39 -0700

    Add files via upload

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass
 function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/vvsonic/Freshly/archive/main.zip" -OutFile "C:\Freshly\PCdeploy.zip"
-Expand-Archive C:\Freshly\PCdeploy.zip -DestinationPath C:\Freshly\
+Invoke-WebRequest -Uri "https://github.com/vvsonic/Freshly/archive/main.zip" -OutFile "C:\Freshly\Freshly.zip"
+Expand-Archive C:\Freshly\Freshly.zip -DestinationPath C:\Freshly\
 }
 downloadbuilder
