7d9eef006e5f097c14e8112addfa3f29d2ea50e1
Sonic Systems <73136664+vvsonic@users.noreply.github.com>
2025-08-04 11:51:18 -0700

    Update PCSetup.ps1
    Disabled the "Disable Action Center" tweak per User feedback

--- PCSetup.ps1
+++ PCSetup.ps1
@@ -59,12 +59,12 @@ function ReclaimWindows
     ##########
 
     # Disable Action Center
-    Write-Host "Disabling Action Center..."
-    If (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer")) {
-      New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" | Out-Null
-    }
-    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
-    Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
+    #Write-Host "Disabling Action Center..."
+    #If (!(Test-Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer")) {
+    #  New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" | Out-Null
+    #}
+    #Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+    #Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
 
     # Disable Autoplay
     Write-Host "Disabling Autoplay..."
